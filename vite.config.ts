8301872bb3eb17bbe5a57df4567cc3c7dc1e7f49
edcamfn2187 <edcamfn2187@gmail.com>
2026-01-06 10:17:11 -0300

    correção Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    host: true, 
-    port: 3000,
-    strictPort: true,
-  },
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-    },
-  },
+  base: "./",
 });
